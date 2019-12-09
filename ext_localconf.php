a58ba60009593b9abd1919d80dc328c1ea63f54f
Florian Wessels <f.wessels@bitmotion.de>
2019-12-09 11:59:40 +0100

    [TASK] Add extension key to user func

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
-    function() {
+    function($extensionKey) {
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_page.php']['getPageOverlay'][] = \Bitmotion\Languagemod\Hooks\PageOverlayHook::class;
-    }
+    }, 'languagemod'
 );
