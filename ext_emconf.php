e7bd3393eda124eb54bcc15721a734543420f81b
Florian Wessels <f.wessels@bitmotion.de>
2019-12-09 11:59:16 +0100

    [TASK] Update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,24 +1,12 @@
 <?php
-
-########################################################################
-# Extension Manager/Repository config file for ext "bm_site".
-#
-# Auto generated 16-07-2012 12:14
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['languagemod'] = [
     'title' => 'Language modification',
     'description' => 'Modifies language menu entries and hreflang link tags',
     'version' => '1.0.0',
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
-            'php' => '7.0.0-7.3.99'
+            'typo3' => '9.5.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
