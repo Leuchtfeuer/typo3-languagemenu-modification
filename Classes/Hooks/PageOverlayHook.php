c5fb1283ea2bdb0eb95b2229db90b07eeacb86c1
Florian Wessels <flo.wessels@gmail.com>
2020-07-22 16:34:20 +0200

    [TASK] Update and apply CS

--- Classes/Hooks/PageOverlayHook.php
+++ Classes/Hooks/PageOverlayHook.php
@@ -1,17 +1,15 @@
 <?php
-declare(strict_types = 1);
-namespace Bitmotion\Languagemod\Hooks;
 
-/***
- *
- * This file is part of the "TYPO3 Language Menu Modification" Extension for TYPO3 CMS.
- *
+declare(strict_types=1);
+
+/*
+ * This file is part of the "Language Modification" extension for TYPO3 CMS.
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
- *
- *  (c) 2019 Florian Wessels <f.wessels@bitmotion.de>, Bitmotion GmbH
- *
- ***/
+ * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ */
+
+namespace Bitmotion\Languagemod\Hooks;
 
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Context\Context;
