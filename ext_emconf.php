e609edd37f14e39f5ec273e2741c099003c47163
Florian Wessels <flo.wessels@gmail.com>
2020-07-22 16:30:57 +0200

    [TASK] Set version to 1.1.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF['languagemod'] = [
     'title' => 'Language Modification',
     'description' => 'Modifies language menu entries and hreflang link tags',
-    'version' => '1.0.1',
+    'version' => '1.1.0-dev',
     'category' => 'fe',
     'constraints' => [
         'depends' => [
