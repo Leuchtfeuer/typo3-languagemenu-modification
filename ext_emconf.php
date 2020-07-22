93c38b5fbeb3411e67c2e2b3bedea64a8c2e52a4
Florian Wessels <flo.wessels@gmail.com>
2020-07-22 16:31:21 +0200

    [FEATURE] Introduce TYPO3 v10 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF['languagemod'] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '10.4.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
