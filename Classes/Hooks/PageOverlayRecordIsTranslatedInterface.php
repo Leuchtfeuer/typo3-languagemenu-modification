095cd6c80d6d131a8de2a003df6f57f5a60fd3b0
Florian Wessels <flo.wessels@gmail.com>
2021-06-14 16:30:36 +0200

    [TASK] Update php-cs-fixer to version 3

--- Classes/Hooks/PageOverlayRecordIsTranslatedInterface.php
+++ Classes/Hooks/PageOverlayRecordIsTranslatedInterface.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 /*
  * This file is part of the "Language Modification" extension for TYPO3 CMS.
+ *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
+ *
  * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
  */
 
