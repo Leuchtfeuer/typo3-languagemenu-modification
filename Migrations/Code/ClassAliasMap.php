f742445eb018db4a275eb046086ab6543370e1c7
Florian Wessels <flo.wessels@gmail.com>
2021-06-14 16:33:20 +0200

    [TASK] Add migration classes

---
+++ Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the "Language Modification" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ */
+
+return [
+    '\Bitmotion\Languagemod\Hooks\PageOverlayHook' => \Leuchtfeuer\Languagemod\Hooks\PageOverlayHook::class,
+    '\Bitmotion\Languagemod\Hooks\PageOverlayRecordIsTranslatedInterface' => \Leuchtfeuer\Languagemod\Hooks\PageOverlayRecordIsTranslatedInterface::class,
+];
---
+++ Migrations/Code/LegacyClassesForIde.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the "Language Modification" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ */
+
+namespace {
+    die('Access denied');
+}
+
+namespace Bitmotion\Languagemod\Hooks {
+
+    /** @deprecated  */
+    abstract class PageOverlayHook extends \Leuchtfeuer\Languagemod\Hooks\PageOverlayHook
+    {
+    }
+
+    /** @deprecated  */
+    interface PageOverlayRecordIsTranslatedInterface extends \Leuchtfeuer\Languagemod\Hooks\PageOverlayRecordIsTranslatedInterface
+    {
+    }
+}
